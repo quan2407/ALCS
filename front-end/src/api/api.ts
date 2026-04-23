ea80e23a9a145a06693dbac41bc51498a302ab51
quan2407 <quannm24072003@gmail.com>
2026-04-23 15:45:13 +0700

    feat(auth): complete frontend authentication flow with interceptor and protected routes

--- front-end/src/api/api.ts
+++ front-end/src/api/api.ts
@@ -7,4 +7,50 @@ const api = axios.create({
   },
 });
 
+// ====== Request Interceptor======
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+    if (
+    token &&
+    !config.url?.includes("/auth/")
+  ) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// ====== Response Interceptor======
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originRequest = error.config;
+    if (error.response.status === 401 && !originRequest._retry) {
+      originRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        if (!refreshToken) {
+          window.location.href = "/login";
+          return;
+        }
+
+        // gọi refresh token
+        const res = await axios.post("http://localhost:8080/api/v1/auth/refresh-token", {
+          refreshToken
+        });
+        const { access_token, refresh_token } = res.data.data;
+        localStorage.setItem("accessToken", access_token);
+        localStorage.setItem("refreshToken", refresh_token);
+        originRequest.headers.Authorization = `Bearer ${access_token}`;
+        return api.request(originRequest);
+      } catch (err) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
